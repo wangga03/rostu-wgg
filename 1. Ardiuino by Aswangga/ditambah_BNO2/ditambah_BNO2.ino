16a11558fbf5613dded2c04bd8bd93c61b8fca7d
wangga03 <151930103+wangga03@users.noreply.github.com>
2024-02-06 13:36:53 +0700

    Update ditambah_BNO2.ino
    halo

--- 1. Ardiuino by Aswangga/ditambah_BNO2/ditambah_BNO2.ino
+++ 1. Ardiuino by Aswangga/ditambah_BNO2/ditambah_BNO2.ino
@@ -23,15 +23,22 @@ volatile int lastState_yA;
 volatile int lastState_yB;
 volatile int lastState_xA;
 volatile int lastState_xB;
-volatile long counter_y = 0;
-volatile long counter_x = 0;
-int lastCounter_x = 0;
-int lastCounter_y = 0;
+volatile long counter_A = 0;
+volatile long counter_B = 0;
+int lastCounter_A = 0;
+
+int lastCounter_B = 0;
+
 const int resolusi = 400;
-float degrees_y;
-float degrees_x;
-float jarak_y = 0;
-float jarak_x = 0;
+float degrees_yA;
+float degrees_xA;
+float degrees_B;
+
+float jarak_yA = 0;
+float jarak_xA = 0;
+float jarak_yB = 0;
+float jarak_xB = 0;
+
 float last_jarak_y;
 float last_jarak_x;
 float reset_jarak;
@@ -40,20 +47,25 @@ float arah_robot = 0;
 float last_arah = 0, last_robot;
 float error_arah = 0;
 float arah_robot_koreksi;
-float x;
-float y;
-float last_y = 0;
-float last_x = 0;
+
+float xA;
+float yA;
+float xB;
+float yB;
+
+float last_yA = 0;
+float last_xA = 0;
+float last_yB = 0;
+float last_xB = 0;
+
 float koordinat_y = 0;
 float koordinat_x = 0;
 
-float old_last_y = 0;
-float old_last_x = 0;
+float temp_xA, temp_yA, temp_yB, temp_xB;
 
-  float temp_x, temp_y;
 
-// Kicker 
 
+int kick;
 void baca_kick_data( const std_msgs::UInt8& kick_data) {
   kick = int(kick_data.data);
   if (kick == 10) { //charging
@@ -83,7 +95,8 @@ void baca_kick_data( const std_msgs::UInt8& kick_data) {
 ros::Subscriber<std_msgs::UInt8> sub("topic_kick", & baca_kick_data);
 
 void setup() {
-//  Pin Kicker
+
+  // Set Pin Kicker
 
   pinMode(30, OUTPUT);
   pinMode(32, OUTPUT);
@@ -126,203 +139,133 @@ void setup() {
   }
 }
 
-
-
 void loop() {
 
-  counter_y;
- 
+  counter_A;
+  counter_B;
+  
 //  ========================= BNO 055 =================================
   // Baca data orientasi dari sensor BNO055
   sensors_event_t event;
   bno.getEvent(&event);
-  arah_robot = event.orientation.x ;//+ 90.0;
-
-//  if (arah_robot > 360.0) {
-//    arah_robot -= 360.0;
-//  } else if (arah_robot < 0.0) {
-//    arah_robot += 360.0;
-//  }
+  arah_robot = event.orientation.x ;
+  
   error_arah = last_arah - arah_robot;
-//  arah_robot_koreksi = last_arah;
+  arah_robot_koreksi = last_arah;
 
 // ================= VECTOR ==================
 
   if (arah_robot - last_arah >= 10 || arah_robot - last_arah <= -10) {
       // Tampilkan nilai orientasi hanya jika perubahannya melebihi +/- 5 derajat
-     
+      
       // Perbarui nilai roll sebelumnya
       last_arah = arah_robot;
     }
 
     sudut.data = last_arah;
 
-// ======================= BATAS VECTOR ==========================
+
+  if (last_arah != arah_robot && (error_arah <= 5 || error_arah >= -5) || (lastCounter_A == counter_A && lastCounter_B == counter_B)){
 
 
- 
- 
-//  ===============================================================================
-// ================ MENGHITUNG KELILING LINGKARAN BERDASARKAN SUDUT ==================
- 
-
-//if (counter_y == lastCounter_y){
-//
-//  counter_y = 0;
-////    lastCounter_y =0;
-////    lastCounter_x =0;
-//    
-//}
-
-////  ================ MENGECEK APAKAH BELOK =============================
-
-  if (last_arah != arah_robot && (error_arah <= 5 || error_arah >= -5) || (lastCounter_y == counter_y)){
-
-
-  counter_y = 0;
-  lastCounter_y = 0;
-  lastCounter_x = 0;
-//  degrees_y = 360.0 * (lastCounter_y / (float)resolusi);
-//  jarak_y = 0;
-//  degrees_x = 360.0 * (lastCounter_x / (float)resolusi);
-//  jarak_x = 0;
-
- 
- 
+    counter_A = 0;
+    lastCounter_A = 0;
+//    lastCounter_A = 0;
+  
+    counter_B = 0;
+    lastCounter_B = 0;
   }
 
-  jarak_x = 0;
-  jarak_y = 0;
-  degrees_y = 360.0 * (lastCounter_y / (float)resolusi);
-  jarak_y = 3.14 * 5.8 * (degrees_y / 360) ;//*  cos(last_arah * PI / 180);;
-  degrees_x = 360.0 * (lastCounter_y / (float)resolusi);
-  jarak_x = 3.14 * 5.8 * (degrees_x / 360) ;//* cos(last_arah * PI / 180);;
-//  y = jarak_y * sin(last_arah* PI/180);
-//  x = jarak_x * cos(last_arah * PI / 180);
+  jarak_xA = 0;
+  jarak_yA = 0;
+  jarak_xB = 0;
+  jarak_yB = 0;
+  
+  degrees_yA = 360.0 * (lastCounter_A / (float)resolusi);
+  jarak_yA = 3.14 * 5.8 * (degrees_yA / 360) ;//*  cos(last_arah * PI / 180);;
+  degrees_xA = 360.0 * (lastCounter_A / (float)resolusi);
+  jarak_xA = 3.14 * 5.8 * (degrees_xA / 360) ;//* cos(last_arah * PI / 180);;
 
-    y = jarak_y * cos(last_arah* PI/180) ;
-    x = jarak_x * sin(last_arah * PI / 180) ;
- 
-//  if(( degrees_y != 0 || degrees_x != 0)){
-//    
-//    
-//    last_y = y;
-//    last_x = x;
-//  }
+  degrees_B = 360.0 * (lastCounter_B / (float)resolusi);
+  jarak_yB = 3.14 * 10 * (degrees_B / 360) ;//*  cos(last_arah * PI / 180);;
+  jarak_xB = 3.14 * 10 * (degrees_B/ 360) ;//* cos(last_arah * PI / 180);;
 
-  float tempSebelumDItambah, nilai_y;
-  if ((y != 0 && x != 0 ) ){
 
- 
-  tempSebelumDItambah = last_y;
-  nilai_y = y;
-  last_y = temp_y + y ;
-  last_x = temp_x + x ;
-  float nilaiLast_y = last_y;
+
+    yA = jarak_yA * cos(last_arah* PI/180) ;
+    xA = jarak_xA * sin(last_arah * PI / 180) ;
+
+    yB = jarak_yB * sin(last_arah * PI/180);
+    xB = jarak_xB *cos(last_arah * PI/180); 
+    
+  float tempSebelumDItambah, nilai_yA;
+  if ((yA != 0 && xA != 0 )  ){
+//    tempSebelumDItambah = last_yA;
+//    nilai_yA = yA;
+    last_yA = temp_yA + yA ;
+    last_xA = temp_xA + xA ;
+
+    
+    
+//    float nilaiLast_yA = last_yA;
   }
-  if (lastCounter_y == counter_y){
-  temp_y = last_y;
-  temp_x = last_x;
+
+  if ((yB != 0 && xB != 0)){
+
+    last_yB = temp_yB + yB ;
+    last_xB = temp_xB + xB ;
+    
+  }
+  if (lastCounter_A == counter_A ){
+    temp_yA = last_yA;
+    temp_xA = last_xA;
   }
 
-  Serial.print("nilai temp sebelum ditambah : ");
-  Serial.println(tempSebelumDItambah);
-  Serial.print("nilai y : ");
-  Serial.println(nilai_y);
-  Serial.print(" cm_y : ");
-  Serial.println(last_y);
-  Serial.print(" cm_x : ");
-  Serial.println(last_x);
+  if( lastCounter_B == counter_B){
+    temp_yB = last_yB;
+    temp_xB = last_xB;
+  }
 
+  koordinat_y = last_yA + last_yB;
+  koordinat_x = last_xA + last_xB;
 
-//  Serial.println("ini di eksekusi");
-//  Serial.print(last_y);
-//  Serial.println(" cm_y");
+//  Serial.print("nilai temp sebelum ditambah : ");
+//  Serial.println(tempSebelumDItambah);
+//  Serial.print("nilai y : ");
+//  Serial.println(nilai_yA);
+//  Serial.print("nilai counter_B : ");
+//  Serial.println(counter_A);
 
-//  Serial.print(counter_x);
-//  Serial.println(" pulse_x");
-//  Serial.print(degrees_x);
-//  Serial.println(" derajat_x");
-//  Serial.print(last_x);
-//  Serial.println(" cm_x");
-//  }
+  Serial.print(" y : ");
+  Serial.println(koordinat_y);
+  Serial.print(" x : ");
+  Serial.println(koordinat_x);
+
 
 //  =================== MENAMPILKAN KOORDINAT =======================
 
-//if (last_robot != last_arah){
 // Tampilkan nilai arah di Serial Monitor
   Serial.print("Arah (dalam derajat): ");
   Serial.println(last_arah);
-//}
- 
-//    Serial.print(counter_y);
-//Serial.print(lastCounter_y);
-//  Serial.println(" pulse_y");
-//  Serial.print(jarak_y);
-//  Serial.println(" jarak y");
- 
+  
+  lastCounter_A = counter_A;
+//  counter_xA = counter_yA;
 
-//
-//  if(counter_y == 0){
-//
-//    temp_y = 0;
-//    temp_x = 0;
-//
-//    temp_y = last_y;
-//    temp_x = last_x;
-//    
-//  }
-//
-//    last_y = 0;
-//    last_x = 0;
- 
-//  =============================================================
-////  ================ MENGECEK APAKAH BELOK =============================
-//
-//  if (last_arah == arah_robot && (error_arah <= 5 || error_arah >= -5 ) && (lastCounter_y <= 1 || lastCounter_y >= -1)){
-//
-//    
-////    lastCounter_y = lastCounter_y;
-////    lastCounter_x = lastCounter_x;
-//    counter_y = 0;
-//    
-//    
-//    
-//  }else{
-//
-//    lastCounter_y = counter_y;
-//    lastCounter_x = counter_y;
-//    
-//  }
- 
-  lastCounter_y = counter_y;
-//  counter_y = lastCounter_y;
-  counter_x = counter_y;
-
-//  
-//  Serial.println("==========");
-//  Serial.print(counter_y);
-//  Serial.println("==========");
-//  Serial.print(counter_x);
-//  Serial.println("==========");
- 
-//   ====================================================================
+  lastCounter_B = counter_B;
 
 // Data Terakhir
-//  last_arah = arah_robot;
+  last_arah = arah_robot;
 
   last_robot = last_arah;
 
 
 
   // Terbitkan data ke ROS
-  hasil.x = last_x;
-  hasil.y = last_y;
+  hasil.x = koordinat_x;
+  hasil.y = koordinat_y;
   positioning_encoder.publish(&hasil);
   sudut_bno.publish(&sudut);
 
- 
- 
 
   // Tunda sejenak untuk menghindari pembacaan berulang
   delay(500);
@@ -330,7 +273,7 @@ void loop() {
 
 void updateCounter_y() {
 
- 
+  
   int currentState_yA = digitalRead(pin_yA);
   int currentState_yB = digitalRead(pin_yB);
   int currentState_xA = digitalRead(pin_xA);
@@ -339,10 +282,10 @@ void updateCounter_y() {
   // Deteksi perubahan
   if ((currentState_yA != lastState_yA || currentState_yB != lastState_yB)) {
     if (currentState_yA == currentState_yB) {
-      counter_y++;
+      counter_A++;
     }
     else {
-      counter_y--;
+      counter_A--;
     }
   }
 
@@ -363,10 +306,10 @@ void updateCounter_x() {
   // Deteksi perubahan
   if ((currentState_xA != lastState_xA || currentState_xB != lastState_xB)) {
     if (currentState_xA == currentState_xB) {
-      counter_x++;
+      counter_B++;
     }
     else {
-      counter_x--;
+      counter_B--;
     }
   }
 
@@ -376,18 +319,4 @@ void updateCounter_x() {
   lastState_yB = currentState_yB;
   lastState_xA = currentState_xA;
   lastState_xB = currentState_xB;
-}
-
-float hitungKoordinat(float x, float y){
-  float current_x, current_y, koreksi_x, koreksi_y;
-
-if (current_y != y || current_x != x){
-  koreksi_x = koreksi_y + x;
-  koreksi_y = koreksi_x + y;
-}
-  current_x = x;
-  current_y = y;
-
-  return koreksi_x, koreksi_y;
- 
 }
